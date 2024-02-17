ae3869539e0b0032e6c3ea20cc4ae5e6192d3d5e
Ashwanth2310 <ashwanth@rajakumar.net>
2024-02-17 12:55:39 +0530

    mongodb updates

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
-// define the ping route
+const mongoURI = process.env.DATABASE_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB :)');
+  })
+  .catch((error) => {
+    console.error('Couldnt connect to MongoDB :(', error);
+  });
 
 app.get('/ping',(req,res)=> {
   res.send("pong");
 });
+
+app.get('/',(req,res) =>{
+  const dbStatus = mongoose.connection.readyState === 1? 'Connected' : 'Disconnected';
+  res.send(`Database Status: ${dbStatus}`);
+})
 
 if (require.main === module) {
   app.listen(port, () => {
